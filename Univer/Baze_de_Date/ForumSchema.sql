d8f76fddc3a0ac026f10a7af9114a7481dd6a928
Polijen <victor4rushwars@gmail.com>
2025-05-13 10:52:46 +0300

    Update ForumSchema.sql

--- Univer/Baze_de_Date/ForumSchema.sql
+++ Univer/Baze_de_Date/ForumSchema.sql
@@ -1,41 +1,5 @@
 CREATE TABLE fDomeniu (
     codD VARCHAR(10) PRIMARY KEY,
-    denumire VARCHAR(100),
-    descriere VARCHAR(255)
-);
-
-CREATE TABLE fAutor (
-    codAutor VARCHAR(10) PRIMARY KEY,
-    nume VARCHAR(100),
-    email VARCHAR(100)
-);
-
-CREATE TABLE fPostare (
-    codP INT PRIMARY KEY,
-    titlu VARCHAR(255),
-    codAutor VARCHAR(10),
-    codD VARCHAR(10),
-    nLike INT,
-    continut VARCHAR(1000),
-    FOREIGN KEY (codAutor) REFERENCES fAutor(codAutor),
-    FOREIGN KEY (codD) REFERENCES fDomeniu(codD)
-);
-
-CREATE TABLE fComentariu (
-    codP INT,
-    codAutor VARCHAR(10),
-    continut VARCHAR(500),
-    nVotDa INT,
-    nVotNu INT,
-    FOREIGN KEY (codP) REFERENCES fPostare(codP),
-    FOREIGN KEY (codAutor) REFERENCES fAutor(codAutor)
-);
-
---new table 
-
-
-CREATE TABLE fDomeniu (
-    codD VARCHAR(10) PRIMARY KEY,
     denumire VARCHAR(100) NOT NULL, 
     descriere VARCHAR(255) DEFAULT 'Nespecificat'
 );
